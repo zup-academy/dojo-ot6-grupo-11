9ebf0802e446a08a2c5cb9a78b10f20c2d8e0726
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-07-28 11:32:51 -0300

    Piloto: Elias Nepomuceno. Co-piloto: Renato Oliveira

---
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/turma/Turma.java
@@ -0,0 +1,32 @@
+package br.com.zupedu.dojo.ot4dojo.turma;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Turma {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true, length = 50)
+	private String nome;
+	private LocalDate dataInicio;
+	private LocalDate dataFim;
+
+	@Deprecated
+	public Turma() {
+	}
+
+	public Turma(String nome, LocalDate dataInicio, LocalDate dataFim) {
+		this.nome = nome;
+		this.dataInicio = dataInicio;
+		this.dataFim = dataFim;
+	}
+}
---
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/turma/TurmaRequest.java
@@ -0,0 +1,33 @@
+package br.com.zupedu.dojo.ot4dojo.turma;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class TurmaRequest {
+
+	@NotBlank
+	@Size(max = 50)
+	private String nome;
+	private LocalDate dataInicio;
+	private LocalDate dataFim;
+	
+	public TurmaRequest(String nome, LocalDate dataInicio, LocalDate dataFim) {
+		this.nome = nome;
+		this.dataInicio = dataInicio;
+		this.dataFim = dataFim;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+	
+	public LocalDate getDataFim() {
+		return dataFim;
+	}
+}
